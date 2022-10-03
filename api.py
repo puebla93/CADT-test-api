22dc2dad5f8bbf21c1d81089504ac50e389c8503
Javier Puebla <jpuebla1993@gmail.com>
2022-10-03 11:09:42 +0200

    refactoring

--- api.py
+++ api.py
@@ -6,29 +6,33 @@ import jsonpickle
 import numpy as np
 from flask import Blueprint, Response, request
 
+from util import apply_filter
 
 api_bp = Blueprint("api", __name__)
 
 
-def apply_filter(img: np.array) -> np.array:
-    return img
-
-
-@api_bp.route("/apply_filter", methods=["POST"])
+@api_bp.route("/apply_filter/", methods=["POST"])
 def apply_filter_handler() -> Response:
-    current_request = request
+    b_image: bytes = request.files["image"].read()
+    img_type: str = request.files["image"].mimetype
+    filter_func: str = request.form["filterFunc"]
+
     # convert string of image data to uint8
-    nparr = np.fromstring(current_request.data, np.uint8)
+    nparr = np.fromstring(b_image, np.uint8)
     # decode image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
     # do some fancy processing here....
-    filtered_img = apply_filter(img)
-    _, img_encoded = cv2.imencode('.png', filtered_img)
+    filtered_img = apply_filter(img, filter_func)
+
+    ext = f".{img_type.split('/')[1]}"
+    _, img_encoded = cv2.imencode(ext, filtered_img)
 
     # build a response dict to send back to client
     response = {
-        "message": f"image received. size={img.shape[1]}x{img.shape[0]}",
+        "msg": "success",
+        "size": [img.shape[1], img.shape[0]],
+        "type": img_type,
         "filtered_img": img_encoded.tostring()
     }
     # encode response using jsonpickle
